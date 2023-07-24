0a0d1a04c5abffc299eeb1cb0fb05ab616df6abb
tanner-andrulis <81568882+tanner-andrulis@users.noreply.github.com>
2023-07-24 12:06:40 -0400

    More robust version checking

--- share/estimation_plug_ins/dummy_tables/dummy_table.py
+++ share/estimation_plug_ins/dummy_tables/dummy_table.py
@@ -1,4 +1,5 @@
 from accelergy.plug_in_interface.interface import *
+from accelergy.version import input_version_greater_or_equal
 
 
 class DummyTable(AccelergyPlugIn):
@@ -8,22 +9,27 @@ class DummyTable(AccelergyPlugIn):
     It can be used as a template for creating user-defined plug-ins
     The energy values returned by this plug-in is not meaningful
     """
+
     # -------------------------------------------------------------------------------------
     # Interface functions, function name, input arguments, and output have to adhere
     # -------------------------------------------------------------------------------------
     def __init__(self):
         pass
 
-    def primitive_action_supported(self, query: AccelergyQuery) -> AccuracyEstimation:
+    def primitive_action_supported(
+        self, query: AccelergyQuery
+    ) -> AccuracyEstimation:
         class_name = query.class_name
         attributes = query.class_attrs
         action_name = query.action_name
         arguments = query.action_args
-        if attributes.get('technology', None) == -1:
+        if attributes.get("technology", None) == -1:
             return AccuracyEstimation(100)
-        if query.input_file_version < 0.4:
+        if not input_version_greater_or_equal(0.4):
             return AccuracyEstimation(1)
-        self.logger.info('Set attribute "technology" to -1 to use the dummy table')
+        self.logger.info(
+            'Set attribute "technology" to -1 to use the dummy table'
+        )
         return AccuracyEstimation(0)
 
     def estimate_energy(self, query: AccelergyQuery) -> Estimation:
@@ -31,20 +37,26 @@ class DummyTable(AccelergyPlugIn):
         attributes = query.class_attrs
         action_name = query.action_name
         arguments = query.action_args
-        
-        energy_pj = 0 if action_name == 'idle' else 1
-        return Estimation(energy_pj, 'p') # Dummy returns 1 for all non-idle actions
 
-    def primitive_area_supported(self, query: AccelergyQuery) -> AccuracyEstimation:
+        energy_pj = 0 if action_name == "idle" else 1
+        return Estimation(
+            energy_pj, "p"
+        )  # Dummy returns 1 for all non-idle actions
+
+    def primitive_area_supported(
+        self, query: AccelergyQuery
+    ) -> AccuracyEstimation:
         class_name = query.class_name
         attributes = query.class_attrs
         action_name = query.action_name
         arguments = query.action_args
-        if attributes.get('technology', None) == -1:
+        if attributes.get("technology", None) == -1:
             return AccuracyEstimation(100)
-        if query.input_file_version < 0.4:
+        if not input_version_greater_or_equal(0.4):
             return AccuracyEstimation(1)
-        self.logger.info('Set attribute "technology" to -1 to use the dummy table')
+        self.logger.info(
+            'Set attribute "technology" to -1 to use the dummy table'
+        )
         return AccuracyEstimation(0)
 
     def estimate_area(self, query: AccelergyQuery) -> Estimation:
@@ -52,7 +64,7 @@ class DummyTable(AccelergyPlugIn):
         attributes = query.class_attrs
         action_name = query.action_name
         arguments = query.action_args
-        return Estimation(1, 'u^2') # Dummy returns 1 for all non-idle actions
+        return Estimation(1, "u^2")  # Dummy returns 1 for all non-idle actions
 
     def get_name(self) -> str:
-        return 'dummy_table'
+        return "dummy_table"
